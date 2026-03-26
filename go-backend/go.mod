9c0751af29fc70d991a7297bc74deb106185f025
Steve <steve.jevon.r@gmail.com>
2026-03-26 15:10:10 +0700

    init a go-backend branch

---
+++ go-backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/steverahardjo/adk-exp_tracker
+
+go 1.24.5
---
+++ go-backend/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("[Log] start the expense tracker backend")
+
+}
