77c3a27a66b4b8435234d4ce5bbce43940300678
Steve <steve.jevon.r@gmail.com>
2026-04-21 21:05:06 +0700

    delete the frontend, redo

--- website/src/hooks/useExpense.ts
+++
@@ -1,3 +0,0 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { authClient } from '@/lib/auth-client'
-import { expenseAPI } from '../services/api'
--- website/src/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-/*src/lib/auth-client.ts */
-
-export const authClient = {
-  getSession: async () => {
-    const response = await fetch('/api/session')
-    return response.json()
-  },
-}
